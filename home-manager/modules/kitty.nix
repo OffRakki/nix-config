8c6412e0f1280a5689bbed433d93c5ac57131c88
OffRakki <fernandomarques1505@gmail.com>
2025-02-22 20:14:35 -0300

    modified:   home-manager/modules/kitty.nix 	deleted:    #flake.nix#

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -11,7 +11,7 @@
 	    enable_audio_bell = false;
 	    mouse_hide_wait = "-1.0";
 	    window_padding_width = 10;
-	    background_opacity = "0.5";
+	    background_opacity = "0.9";
 	    background_blur = 5;
 	    symbol_map = let
 		mappings = [
