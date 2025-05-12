e6242c0aefddfe4bbeeb97bed89bdbcd9b335eb4
OffRakki <fernandomarques1505@gmail.com>
2025-05-12 19:31:50 -0300

    modified:   home-manager/modules/hyprland.nix 	modified:   home-manager/modules/kitty.nix 	modified:   home-manager/modules/waybar/style.css 	modified:   hosts/rakki/packages-fonts.nix 	modified:   nixos/modules/fish.nix

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -1,252 +1,260 @@
 { inputs, lib, config, pkgs, ... }: {
 
-  wayland.windowManager.hyprland = {
+	wayland.windowManager.hyprland = {
 		enable = true;
 		xwayland.enable = true;
 		plugins = [];
 		systemd = {
-	    enable = true;
-	    variables = ["--all"];
+			enable = true;
+			variables = ["--all"];
 		};
 		settings = {
 
-	    "$mainMod"    = "SUPER";
-	    "$terminal"   = "${pkgs.kitty}/bin/kitty";
-	    "$files"      = "${pkgs.kitty}/bin/kitty -e ${pkgs.ranger}/bin/ranger";
-	    "$qalc" = "${pkgs.qalculate-gtk}/bin/qalculate-gtk";
+			"$mainMod"    = "SUPER";
+			"$terminal"   = "${pkgs.kitty}/bin/kitty";
+			"$files"      = "${pkgs.kitty}/bin/kitty -e ${pkgs.ranger}/bin/ranger";
+			"$qalc" = "${pkgs.qalculate-gtk}/bin/qalculate-gtk";
 
-	    monitor = [ 
-	    	"DP-3,1920x1080@239.76,0x0,1"
-	    	"HDMI-A-1,highres,1920x-250,1"
-				#"HDMI-A-1,disable"
-		];
-
-	    dwindle = {	
-		pseudotile = "yes";
-		preserve_split = "yes";
-		special_scale_factor = 0.8;
-	    };
-
-	    master = {
-		new_status = "master";	
-		new_on_top = 1;
-		mfact = 0.5;
-	    };
-
-	    general = {
-		resize_on_border = true;
-		allow_tearing = true;
-		layout = "dwindle";
-		border_size = 2;
-		gaps_in = 4;
-		gaps_out = 6;
-
-		#"col.active_border" = "$color12";
-		#"col.inactive_border" = "$color10";
-	    };
-
-	    input = {
-		kb_layout = "us";
-		kb_variant = ""; 
-		    kb_model = "";
-		    kb_options = "";
-		    kb_rules = "";
-		    repeat_rate = 50;
-		repeat_delay = 300;
-
-		sensitivity = -0.7;
-		numlock_by_default = true;
-		left_handed = false;
-		follow_mouse = true;
-		float_switch_override_focus = false;
-		tablet = {
-		    transform = 0;
-		    left_handed = 0;
-		};
-	    };
-
-	    misc = {
-		disable_hyprland_logo = true;
-		disable_splash_rendering = true;
-		vfr = true;
-		vrr = 2;
-		mouse_move_enables_dpms = true;
-		enable_swallow = true;
-		focus_on_activate = false;
-		initial_workspace_tracking = 0;
-		middle_click_paste = false;
-	    };
-
-	    binds = {
-		workspace_back_and_forth = true;
-		allow_workspace_cycles = true;
-		pass_mouse_when_bound = false;
-	    };
-
-	    cursor = {
-		no_hardware_cursors = true;
-		enable_hyprcursor = true;
-		warp_on_change_workspace = 2;
-		no_warps = true;
-	    };
-
-	    decoration = { 
-		rounding = 10;
-
-		active_opacity = 2.0;
-		inactive_opacity = 0.9;
-		fullscreen_opacity = 1.0;
-
-		dim_inactive = true;
-		dim_strength = 0.1;
-		dim_special = 0.8;
-
-		shadow = {
-		    enabled = true;
-		    range = 3;
-		    render_power = 1;
-
-		    #"color" = "$color12";
-		    #"color_inactive" = "$color10";
-		};
-
-		blur = {
-		    enabled = true;
-		    size = 6;
-		    passes = 2;
-		    ignore_opacity = true;
-		    new_optimizations = true;
-		    special = true;
-		    popups = true;
-		};
-	    };
-
-	    group = {
-		#"col.border_active" = "$color15";
-
-		groupbar = {
-		    #"col.active" = "$color0";
-		};
-	    };
-
-	    animations = {
-		enabled = "yes, please :)";
-
-		bezier = [ 
-		"myBezier, 0.05, 0.9, 0.1, 1.05"
-		"linear, 0.0, 0.0, 1.0, 1.0"
-		"wind, 0.05, 0.9, 0.1, 1.05"
-		"winIn, 0.1, 1.1, 0.1, 1.1"
-		"winOut, 0.3, -0.3, 0, 1"
-		"slow, 0, 0.85, 0.3, 1"
-		"overshot, 0.7, 0.6, 0.1, 1.1"
-		"bounce, 1.1, 1.6, 0.1, 0.85"
-		"sligshot, 1, -1, 0.15, 1.25"
-		"nice, 0, 6.9, 0.5, -4.20"
-		];
-
-		animation = [ 
-		"windowsIn, 1, 5, slow, popin"
-		"windowsOut, 1, 5, winOut, popin"
-		"windowsMove, 1, 5, wind, slide"
-		"border, 1, 10, linear"
-		"borderangle, 1, 180, linear, loop" #used by rainbow borders and rotating colors
-		"fade, 1, 5, overshot"
-		"workspaces, 1, 5, wind"
-		"windows, 1, 5, bounce, popin"
-		];
-
-	    };
-
-	    env = [ 
-		"NIXOS_OZONE_WL,1"
-		#"EDITOR,nvim #default editor"
-		"CLUTTER_BACKEND,wayland"
-		"GDK_BACKEND,wayland,x11"
-		"QT_AUTO_SCREEN_SCALE_FACTOR,1"
-		"QT_QPA_PLATFORM,wayland"
-		"QT_QPA_PLATFORMTHEME,qt5ct"
-		"QT_QPA_PLATFORMTHEME,qt6ct   "
-		"QT_SCALE_FACTOR,1"
-		"QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-		"XDG_CURRENT_DESKTOP,Hyprland"
-		"XDG_SESSION_DESKTOP,Hyprland"
-		"XDG_SESSION_TYPE,wayland"
-		"QT_QUICK_CONTROLS_STYLE,org.hyprland.style"
-		"ELECTRON_OZONE_PLATFORM_HINT,auto"
-		"LIBVA_DRIVER_NAME,nvidia "
-		"__GLX_VENDOR_LIBRARY_NAME,nvidia"
-		"NVD_BACKEND,direct "
-		"AQ_DRM_DEVICES,/dev/dri/card1"
-		"GBM_BACKEND,nvidia-drm "
-		"__NV_PRIME_RENDER_OFFLOAD,1 "
-		"__VK_LAYER_NV_optimus,NVIDIA_only"
-		"WLR_DRM_NO_ATOMIC,1"
-		"LIBGL_ALWAYS_SOFTWARE,1 # Warning. May cause hyprland to crash"
-		"WLR_RENDERER_ALLOW_SOFTWARE,1"
-		"MOZ_DISABLE_RDD_SANDBOX,1"
-		"EGL_PLATFORM,wayland"
-		"GDK_SCALE,1 "
-		"HYPRCURSOR_THEME,Bibata-Modern-Ice"
-		"HYPRCURSOR_SIZE,24"
-		"MOZ_ENABLE_WAYLAND,1"
-		];
-
-	    exec-once = [ 
-		"swww-daemon --format xrgb"
-		"$SwwwRandom $wallDIR # random wallpaper switcher every 30 minutes "
-		"dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-		"systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-		"$scriptsDir/Polkit.sh"
-		"nm-applet --indicator &"
-		"swaync &"
-		"ags &"
-		"blueman-applet & "
-		"rog-control-center &"
-		"wl-paste --type text --watch cliphist store "
-		"wl-paste --type image --watch cliphist store"
-		#"$UserScripts/RainbowBorders.sh &"
-		"hypridle &"
-		"pypr &"
-		"swww-daemon --format xrgb && swww img $HOME/Pictures/wallpapers/mecha-nostalgia.png  # persistent wallpaper"
-		"$scriptsDir/Polkit-NixOS.sh"
-	    ];
-
-			binde = [
-				"$mainMod SHIFT, left, resizeactive,-50 0"
-				"$mainMod SHIFT, right, resizeactive,50 0"
-				"$mainMod SHIFT, up, resizeactive,0 -50"
-				"$mainMod SHIFT, down, resizeactive,0 50"
+			monitor = [ 
+				"DP-3,1920x1080@239.76,0x0,1"
+				"HDMI-A-1,highres,1920x-250,1"
+#"HDMI-A-1,disable"
 			];
 
-	    bindr = [ 
-				"$mainMod, $mainMod_L, exec, pkill rofi || rofi -show drun -modi drun,filebrowser,run,window" # Super Key to Launch rofi menu
-	    ];
+			workspace = [
+				"1, monitor:DP-3"
+				"2, monitor:HDMI-A-1"
+			];
 
-	    bindn = [ 
+			dwindle = {	
+				pseudotile = "yes";
+				preserve_split = "yes";
+				special_scale_factor = 0.8;
+			};
+
+			master = {
+				new_status = "master";	
+				new_on_top = 1;
+				mfact = 0.5;
+			};
+
+			general = {
+				resize_on_border = true;
+				allow_tearing = true;
+				layout = "dwindle";
+				border_size = 2;
+				gaps_in = 4;
+				gaps_out = 6;
+
+				"col.active_border" = "rgba(fe640baa) rgba(df8e1daa) 45deg";
+				"col.inactive_border" = "rgb(dd7878)";
+			};
+
+			input = {
+				kb_layout = "us";
+				kb_variant = ""; 
+				kb_model = "";
+				kb_options = "";
+				kb_rules = "";
+				repeat_rate = 50;
+				repeat_delay = 300;
+
+				sensitivity = -0.7;
+				numlock_by_default = true;
+				left_handed = false;
+				follow_mouse = true;
+				float_switch_override_focus = false;
+				tablet = {
+					transform = 0;
+					left_handed = 0;
+				};
+			};
+
+			misc = {
+				disable_hyprland_logo = true;
+				disable_splash_rendering = true;
+				vfr = true;
+				vrr = 2;
+				mouse_move_enables_dpms = true;
+				enable_swallow = true;
+				focus_on_activate = false;
+				initial_workspace_tracking = 0;
+				middle_click_paste = false;
+			};
+
+			binds = {
+				workspace_back_and_forth = true;
+				allow_workspace_cycles = true;
+				pass_mouse_when_bound = false;
+			};
+
+			cursor = {
+				no_hardware_cursors = true;
+				enable_hyprcursor = true;
+				warp_on_change_workspace = 2;
+				no_warps = true;
+			};
+
+			decoration = { 
+				rounding = 6;
+
+				active_opacity = 2.0;
+				inactive_opacity = 0.9;
+				fullscreen_opacity = 1.0;
+
+				dim_inactive = true;
+				dim_strength = 0.1;
+				dim_special = 0.8;
+
+				shadow = {
+					enabled = true;
+					range = 3;
+					render_power = 1;
+
+#"color" = "$color12";
+#"color_inactive" = "$color10";
+				};
+
+				blur = {
+					enabled = true;
+					size = 6;
+					passes = 2;
+					ignore_opacity = true;
+					new_optimizations = true;
+					special = true;
+					popups = true;
+				};
+			};
+
+			group = {
+#"col.border_active" = "$color15";
+
+				groupbar = {
+#"col.active" = "$color0";
+				};
+			};
+
+			animations = {
+				enabled = "yes, please :)";
+
+				bezier = [ 
+					"myBezier, 0.05, 0.9, 0.1, 1.05"
+					"linear, 0.0, 0.0, 1.0, 1.0"
+					"wind, 0.05, 0.9, 0.1, 1.05"
+					"winIn, 0.1, 1.1, 0.1, 1.1"
+					"winOut, 0.3, -0.3, 0, 1"
+					"slow, 0, 0.85, 0.3, 1"
+					"overshot, 0.7, 0.6, 0.1, 1.1"
+					"bounce, 1.1, 1.6, 0.1, 0.85"
+					"sligshot, 1, -1, 0.15, 1.25"
+					"nice, 0, 6.9, 0.5, -4.20"
+				];
+
+				animation = [ 
+					"windowsIn, 1, 5, slow, popin"
+					"windowsOut, 1, 5, winOut, popin"
+					"windowsMove, 1, 5, wind, slide"
+					"border, 1, 2, linear"
+					"borderangle, 1, 180, linear, loop" #used by rainbow borders and rotating colors
+					"fade, 1, 5, overshot"
+					"workspaces, 1, 5, wind"
+					"windows, 1, 5, bounce, popin"
+				];
+			};
+
+			windowrule = [
+				"noblur, tag:games*"
+			];
+
+			env = [ 
+				"NIXOS_OZONE_WL,1"
+				"EDITOR,neovim #default editor"
+				"CLUTTER_BACKEND,wayland"
+				"GDK_BACKEND,wayland,x11"
+				"QT_AUTO_SCREEN_SCALE_FACTOR,1"
+				"QT_QPA_PLATFORM,wayland"
+				"QT_QPA_PLATFORMTHEME,qt5ct"
+				"QT_QPA_PLATFORMTHEME,qt6ct   "
+				"QT_SCALE_FACTOR,1"
+				"QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+				"XDG_CURRENT_DESKTOP,Hyprland"
+				"XDG_SESSION_DESKTOP,Hyprland"
+				"XDG_SESSION_TYPE,wayland"
+				"QT_QUICK_CONTROLS_STYLE,org.hyprland.style"
+				"ELECTRON_OZONE_PLATFORM_HINT,auto"
+				"LIBVA_DRIVER_NAME,nvidia "
+				"__GLX_VENDOR_LIBRARY_NAME,nvidia"
+				"NVD_BACKEND,direct "
+				"AQ_DRM_DEVICES,/dev/dri/card1"
+				"GBM_BACKEND,nvidia-drm "
+				"__NV_PRIME_RENDER_OFFLOAD,1 "
+				"__VK_LAYER_NV_optimus,NVIDIA_only"
+				"WLR_DRM_NO_ATOMIC,1"
+				"LIBGL_ALWAYS_SOFTWARE,1 # Warning. May cause hyprland to crash"
+				"WLR_RENDERER_ALLOW_SOFTWARE,1"
+				"MOZ_DISABLE_RDD_SANDBOX,1"
+				"EGL_PLATFORM,wayland"
+				"GDK_SCALE,1 "
+				"HYPRCURSOR_THEME,Bibata-Modern-Ice"
+				"HYPRCURSOR_SIZE,24"
+				"MOZ_ENABLE_WAYLAND,1"
+				];
+
+			exec-once = [ 
+				"swww-daemon --format xrgb"
+#"$SwwwRandom $wallDIR # random wallpaper switcher every 30 minutes "
+				"dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+				"systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+#"$scriptsDir/Polkit.sh"
+				"nm-applet --indicator &"
+				"swaync &"
+				"ags &"
+				"blueman-applet & "
+				"rog-control-center &"
+				"wl-paste --type text --watch cliphist store "
+				"wl-paste --type image --watch cliphist store"
+#"$UserScripts/RainbowBorders.sh &"
+				"hypridle &"
+				"pypr &"
+				"swww-daemon --format xrgb && swww img $HOME/Downloads/Kath.png  # persistent wallpaper"
+#"$scriptsDir/Polkit-NixOS.sh"
+			];
+
+			binde = [
+				"$mainMod CTRL, left, resizeactive,-50 0"
+					"$mainMod CTRL, right, resizeactive,50 0"
+					"$mainMod CTRL, up, resizeactive,0 -50"
+					"$mainMod CTRL, down, resizeactive,0 50"
+			];
+
+			bindr = [ 
+				"$mainMod, $mainMod_L, exec, pkill rofi || rofi -show drun -modi drun,filebrowser,run,window" # Super Key to Launch rofi menu
+			];
+
+			bindn = [ 
 				"ALT_L, SHIFT_L, exec, $scriptsDir/SwitchKeyboardLayout.sh" # Change keyboard layout
-	    ];
+			];
 
 			bindm = [
 				"$mainMod, mouse:272, movewindow # NOTE: mouse:272 = left click"
-				"$mainMod, mouse:273, resizewindow # NOTE: mouse:272 = right click"
+					"$mainMod, mouse:273, resizewindow # NOTE: mouse:272 = right click"
 			];
 
-			#bindl = [
-			#	"bindl = , xf86AudioPlayPause, exec, $scriptsDir/MediaCtrl.sh --pause"
-			#	"bindl = , xf86AudioPause, exec, $scriptsDir/MediaCtrl.sh --pause"
-			#	"bindl = , xf86AudioPlay, exec, $scriptsDir/MediaCtrl.sh --pause"
-			#	"bindl = , xf86AudioNext, exec, $scriptsDir/MediaCtrl.sh --nxt "
-			#	"bindl = , xf86AudioPrev, exec, $scriptsDir/MediaCtrl.sh --prv"
-			#	"bindl = , xf86audiostop, exec, $scriptsDir/MediaCtrl.sh --stop"
-			#];
+#bindl = [
+#	"bindl = , xf86AudioPlayPause, exec, $scriptsDir/MediaCtrl.sh --pause"
+#	"bindl = , xf86AudioPause, exec, $scriptsDir/MediaCtrl.sh --pause"
+#	"bindl = , xf86AudioPlay, exec, $scriptsDir/MediaCtrl.sh --pause"
+#	"bindl = , xf86AudioNext, exec, $scriptsDir/MediaCtrl.sh --nxt "
+#	"bindl = , xf86AudioPrev, exec, $scriptsDir/MediaCtrl.sh --prv"
+#	"bindl = , xf86audiostop, exec, $scriptsDir/MediaCtrl.sh --stop"
+#];
 
-	    bind = [ 
+			bind = [ 
 				"$mainMod, D, exec, pkill rofi || rofi -show drun -modi drun,filebrowser,run,window" #Main Menu
 				"$mainMod, SPACE, togglefloating"
-				"$mainMod CTRL, F, fullscreen, 1 # fake full screen"
+				"$mainMod SHIFT, F, fullscreen, 1 # fake full screen"
 				"$mainMod, F, fullscreen"
-    		"$mainMod SHIFT, Q, killactive"
+				"$mainMod SHIFT, Q, killactive"
 				"$mainMod, A, exec, pkill rofi || true && ags -t 'overview'"
 				"$mainMod, Return, exec, $terminal"  #terminal
 				"$mainMod, T, exec, $files" #file manager
@@ -255,7 +263,7 @@
 				"$mainMod, Z, exec, pypr zoom # Toggle Desktop Zoom"
 				"$mainMod SHIFT, O, exec, $UserScripts/ZshChangeTheme.sh" # Change oh-my-zsh theme
 
-				# Switch workspaces with mainMod + [0-9] 
+# Switch workspaces with mainMod + [0-9] 
 				"$mainMod, code:10, workspace, 1 # NOTE: code:10 = key 1"
 				"$mainMod, code:11, workspace, 2 # NOTE: code:11 = key 2"
 				"$mainMod, code:12, workspace, 3 # NOTE: code:12 = key 3"
@@ -267,7 +275,7 @@
 				"$mainMod, code:18, workspace, 9 # NOTE: code:18 = key 9"
 				"$mainMod, code:19, workspace, 10 # NOTE: code:19 = key 0"
 
-				# Move active window and follow to workspace mainMod + SHIFT [0-9]
+# Move active window and follow to workspace mainMod + SHIFT [0-9]
 				"$mainMod SHIFT, code:10, movetoworkspace, 1 # NOTE: code:10 = key 1"
 				"$mainMod SHIFT, code:11, movetoworkspace, 2 # NOTE: code:11 = key 2"
 				"$mainMod SHIFT, code:12, movetoworkspace, 3 # NOTE: code:12 = key 3"
@@ -281,36 +289,36 @@
 				"$mainMod SHIFT, bracketleft, movetoworkspace, -1 # brackets ["
 				"$mainMod SHIFT, bracketright, movetoworkspace, +1 # brackets ]"
 
-				# Move focus with mainMod + arrow keys
+# Move focus with mainMod + arrow keys
 				"$mainMod, left, movefocus, l"
 				"$mainMod, right, movefocus, r"
 				"$mainMod, up, movefocus, u"
 				"$mainMod, down, movefocus, d"
 
-				# Move windows
-				"$mainMod CTRL, left, movewindow, l"
-				"$mainMod CTRL, right, movewindow, r"
-				"$mainMod CTRL, up, movewindow, u"
-				"$mainMod CTRL, down, movewindow, d"
+# Move windows
+				"$mainMod SHIFT, left, movewindow, l"
+				"$mainMod SHIFT, right, movewindow, r"
+				"$mainMod SHIFT, up, movewindow, u"
+				"$mainMod SHIFT, down, movewindow, d"
 
-				# Dwindle Layout
+# Dwindle Layout
 				"$mainMod SHIFT, I, togglesplit # only works on dwindle layout"
 				"$mainMod, P, pseudo, # dwindle"
-	    ];
+				];
 
-		# For passthrough keyboard into a VM
-		# bind = $mainModALT, P, submap, passthru
-		#submap = passthru
-		# to unbind
-		#bind = $mainModALT, P, submap, reset
-		#submap = reset
+# For passthrough keyboard into a VM
+# bind = $mainModALT, P, submap, passthru
+#submap = passthru
+# to unbind
+#bind = $mainModALT, P, submap, reset
+#submap = reset
 
-	#source = [ 
-	#	"$HOME/.config/hypr/wallust/wallust-hyprland.conf"
-	#	"$HOME/.config/hypr/configs/Keybinds.conf"
-	#	"$HOME/.config/hypr/UserConfigs/WindowRules.conf"
-	#	"$HOME/.config/hypr/UserConfigs/WorkspaceRules.conf"
-	#	];
+#source = [ 
+#	"$HOME/.config/hypr/wallust/wallust-hyprland.conf"
+#	"$HOME/.config/hypr/configs/Keybinds.conf"
+#	"$HOME/.config/hypr/UserConfigs/WindowRules.conf"
+#	"$HOME/.config/hypr/UserConfigs/WorkspaceRules.conf"
+#	];
+		};
 	};
-    };
-}
+																		}
