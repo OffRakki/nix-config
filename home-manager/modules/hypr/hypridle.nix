8d443ab112aaaf1f8c1f8b93be4976e729ffc8eb
offrakki <fernandomarques1505@gmail.com>
2025-11-03 17:53:17 -0300

    hopefully working well:   home-manager/modules/hypr/hypridle.nix

--- home-manager/modules/hypr/hypridle.nix
+++ home-manager/modules/hypr/hypridle.nix
@@ -6,12 +6,19 @@
     in {
       general = {
         lock_cmd = "if ! ${isLocked}; then ${lib.getExe config.programs.hyprlock.package} --grace 5; fi";
+        inhibit_sleep = 3;
         after_sleep_cmd = "hyprctl dispatch dpms on";
       };
       listener = [
         {
-          timeout = 300;
+          timeout = 120;
           on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        # If already locked
+        {
+          timeout = 30;
+          on-timeout = "if ${isLocked}; then hyprctl dispatch dpms off; fi";
           on-resume = "hyprctl dispatch dpms on";
         }
       ];
