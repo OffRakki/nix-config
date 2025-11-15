bcedcbc44b16ddd95719a7812f60ecf65743ae18
offrakki <fernandomarques1505@gmail.com>
2025-11-15 13:26:31 -0300

    Customizing jj

--- home-manager/modules/jujutsu.nix
+++ home-manager/modules/jujutsu.nix
@@ -9,6 +9,7 @@
       ui = {
         pager = "less -FRX";
         default-command = "log";
+        graph.style = "curved";
       };
       templates = {
         draft_commit_description = ''
@@ -23,6 +24,22 @@
             )),
             diff.git(),
           )
+        '';
+      };
+      template-aliases = {
+        "format_short_commit_header(commit)" = ''
+          separate(" ",
+            format_short_change_id_with_hidden_and_divergent_info(commit),
+            format_timestamp(commit_timestamp(commit)),
+            commit.bookmarks(),
+            commit.tags(),
+            commit.working_copies(),
+            if(commit.git_head(), label("git_head", "git_head()")),
+            format_short_commit_id(commit.commit_id()),
+            if(commit.conflict(), label("conflict", "conflict")),
+            if(config("ui.show-cryptographic-signatures").as_boolean(),
+              format_short_cryptographic_signature(commit.signature())),
+          )  
         '';
       };
     };
