6dcc3dae39568e6d3ebbe4df8f10cc0f1e3139c7
offrakki <fernandomarques1505@gmail.com>
2025-11-15 14:23:25 -0300

    trying to make hypridle WM aware

--- home-manager/modules/hypr/hypridle.nix
+++ home-manager/modules/hypr/hypridle.nix
@@ -1,4 +1,5 @@
-{config, lib, ...}: {
+{config, lib, pkgs, ...}:
+{
   services.hypridle = {
     enable = true;
     settings = let
@@ -21,6 +22,44 @@
           on-resume = "hyprctl dispatch dpms on";
         }
       ];
-    };
+    };      # (lib.optionals config.programs.niri.enable {
+      #   general = {
+      #     lock_cmd = "if ! ${isLocked}; then hyprlock --grace 5; fi";
+      #     inhibit_sleep = 3;
+      #     after_sleep_cmd = "niri msg output DP-3 on";
+      #   };
+      #   listener = [
+      #     {
+      #       timeout = 120;
+      #       on-timeout = "if ! ${isLocked}; then hyprlock --grace 5; fi";
+      #     }
+      #     # If already locked
+      #     {
+      #       timeout = 720;
+      #       on-timeout = "if ${isLocked}; then niri msg output DP-3 off; fi";
+      #       on-resume = "niri msg output DP-3 on";
+      #     }
+      #   ];
+      # })
+      # ++
+      # (lib.optionals config.wayland.windowManager.hyprland.enable {
+      #   general = {
+      #     lock_cmd = "if ! ${isLocked}; then hyprlock --grace 5; fi";
+      #     inhibit_sleep = 3;
+      #     after_sleep_cmd = "hyprctl dispatch dpms on";
+      #   };
+      #   listener = [
+      #     {
+      #       timeout = 120;
+      #       on-timeout = "if ! ${isLocked}; then hyprlock --grace 5; fi";
+      #     }
+      #     # If already locked
+      #     {
+      #       timeout = 720;
+      #       on-timeout = "if ${isLocked}; then hyprctl dispatch dpms off; fi";
+      #       on-resume = "hyprctl dispatch dpms on";
+      #     }
+      #   ];
+      # });
   };
 }
