d363a5966488ef793e9102c187c026d99ccc4170
offrakki <fernandomarques1505@gmail.com>
2025-12-06 17:20:32 -0300

    removed vm-guest services

--- hosts/rakki/drivers/default.nix
+++ hosts/rakki/drivers/default.nix
@@ -2,9 +2,9 @@
   imports = [
   # ./amd-drivers.nix
   # ./intel-drivers.nix
+    # ./vm-guest-services.nix
     ./nvidia-drivers.nix
     ./nvidia-prime-drivers.nix
-    ./vm-guest-services.nix
     ./local-hardware-clock.nix
   ];
 }
