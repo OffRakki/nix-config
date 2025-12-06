6717fd5f7ddd4997e372eb76bb4ccb821f7f1a99
offrakki <fernandomarques1505@gmail.com>
2025-12-06 17:20:32 -0300

    removed unused wasistlos pkg and not needed pipewire pkg declared

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -107,7 +107,6 @@
   };
   services.mpd.user = "rakki";
   systemd.services.mpd.environment = {
-    # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
     XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.rakki.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
   };
 
@@ -207,12 +206,10 @@
     starship
     fastfetch
     wireplumber
-    pipewire
     pwvucontrol
     pipecontrol
     btop
     qutebrowser
-    wasistlos
     vesktop
 		waybar-mpris
     wl-clipboard-rs
