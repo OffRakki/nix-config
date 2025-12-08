1439c25e20e3b4b12bb49c89de1ab8ce169df4ef
offrakki <fernandomarques1505@gmail.com>
2025-12-08 16:57:54 -0300

    removed spicetify

--- home-manager/modules/spicetify.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-	programs.spicetify = 
-	let
-		spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-	in {
-		enable = true;
-		theme = spicePkgs.themes.gruvbox;
-	};
-}
