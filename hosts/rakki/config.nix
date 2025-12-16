35754d2298f48a01589652002dd8cc48073e5cd7
offrakki <fernandomarques1505@gmail.com>
2025-12-16 19:05:07 -0300

    removed redundant version statement

--- hosts/rakki/config.nix
+++ hosts/rakki/config.nix
@@ -197,12 +197,4 @@
     allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
     allowedUDPPortRanges = [{ from = 1714; to = 1764; }];
   };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
 }
