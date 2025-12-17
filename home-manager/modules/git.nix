a41bddf20fb0952e2e8ee2b2ca8bbd9f022d1388
offrakki <fernandomarques1505@gmail.com>
2025-12-17 09:05:02 -0300

    added libsecret as a pkg to git config

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -6,7 +6,7 @@
 		settings = {
 			core.editor = "hx";
 			init.defaultBranch = "master";
-			credential.helper = "libsecret";
+			credential.helper = "${lib.getExe pkgs.libsecret}";
 			user.name = "offrakki";
 			user.email = "fernandomarques1505@gmail.com";
 			commit.verbose = true;
