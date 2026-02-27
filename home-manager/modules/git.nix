2b14c46f533ee17bc80be58219ba2f1e05f3b9f4
offrakki <offrakki@gmail.com>
2026-02-27 12:16:26 -0300

    changed git email

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -10,7 +10,7 @@
       init.defaultBranch = "master";
       credential.helper = "${lib.getExe pkgs.libsecret}";
       user.name = "offrakki";
-      user.email = "fernandomarques1505@gmail.com";
+      user.email = "offrakki@gmail.com";
       commit.verbose = true;
       column.ui = "auto";
     };
